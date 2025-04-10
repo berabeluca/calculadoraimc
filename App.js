ab9bb12e63477368fe3f1bc14a8c2f45b21aded5
Lucas Mickael <lucasmickael440@gmail.com>
2025-04-10 17:32:42 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from 'react-native';
+import Title from './components/Title';
+import FormIMC from './components/FormIMC';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Title/>
+      <FormIMC/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+});
